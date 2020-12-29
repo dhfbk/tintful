c9128605fad01a4f779f59e34c038a45c3ec7662
valefras <48183554+valefras@users.noreply.github.com>
2020-12-29 17:34:22 +0100

    progress in pos/feats modal

---
+++ src/components/featsSelect.vue
@@ -0,0 +1,37 @@
+<template>
+    <select
+        class="appearance-none font-medium h-full border-b   block appearance-none bg-white border-gray-400 text-gray-700 py-1 pl-2 pr-12 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        :name="opt[0]"
+        :id="opt[0]"
+        @change="send"
+        v-model="md"
+    >
+        <option v-for="(i, x) in opt" :key="x" :value="i" class="">
+            {{ i }}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    props: {
+        opt: Array,
+        model: String,
+    },
+    data() {
+        return {
+            md: '',
+        }
+    },
+    created() {
+        this.md = this.model
+    },
+    methods: {
+        send() {
+            console.log('data changed!')
+        },
+    },
+}
+</script>
+
+<style></style>
--- src/components/featuresModal.vue
+++ src/components/featuresModal.vue
@@ -26,51 +26,35 @@
                                 <span class="sr-only">Close dialog</span>
                             </span>
                         </div>
+                        <span>POS:</span>
+                        <transition name="zoom">
+                            <select
+                                class="appearance-none font-medium h-full border-b   block appearance-none bg-white border-gray-400 text-gray-700 py-1 pl-2 pr-12 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                name="posSelect"
+                                id="posSelect"
+                                v-model="pos"
+                            >
+                                <option v-for="(i, x) in posList" :key="x" :value="i.abbr" class="">
+                                    {{ i.abbr }} ({{ i.full }})
+                                </option>
+                            </select>
+                        </transition>
                         <div class="py-2">
                             <span class="">{{ featsToEdit }} </span>
                             <transition name="zoom">
-                                <select
-                                    class="appearance-none font-medium h-full border-b   block appearance-none bg-white border-gray-400 text-gray-700 py-1 pl-2 pr-12 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                    name="depsSelect"
-                                    id="depsSelect"
-                                    @change="gender"
-                                    v-model="gen"
-                                    v-if="showGen"
-                                >
-                                    <option v-for="(gen, i) in params.Gender" :key="i" :value="gen" class="">
-                                        {{ gen }}
-                                    </option>
-                                </select>
+                                <featsSelect :opt="params.Gender" :model="this.gen" v-if="showGen" />
                             </transition>
-                            <transition name="zoom">
-                                <select
-                                    class="appearance-none font-medium h-full border-b  block appearance-none bg-white border-gray-400 text-gray-700 py-1 pl-2 pr-12 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                    name="depsSelect"
-                                    id="depsSelect"
-                                    @change="number"
-                                    v-model="num"
-                                    v-if="showNum"
-                                >
-                                    <option v-for="(num, i) in params.Number" :key="i" :value="num" class="">
-                                        {{ num }}
-                                    </option>
-                                </select>
-                            </transition>
-                            <transition name="zoom">
-                                <select
-                                    class="appearance-none font-medium h-full border-b  block appearance-none bg-white border-gray-400 text-gray-700 py-1 pl-2 pr-12 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                    name="depsSelect"
-                                    id="depsSelect"
-                                    @change="tense"
-                                    v-model="ten"
-                                    v-if="showTen"
-                                >
-                                    <option v-for="(ten, i) in params.Tense" :key="i" :value="ten" class="">
-                                        {{ ten }}
-                                    </option>
-                                </select>
-                            </transition>
-                            <div @click="showGen = true">show</div>
+                            <featsSelect :opt="params.Number" :model="this.num" v-if="showNum" />
+                            <featsSelect :opt="params.Tense" :model="this.ten" v-if="showTen" />
+                            <featsSelect :opt="params.VerbForm" :model="this.form" v-if="showForm" />
+                            <featsSelect :opt="params.Person" :model="this.per" v-if="showPer" />
+                            <featsSelect :opt="params.Mood" :model="this.mood" v-if="showMood" />
+                            <featsSelect :opt="params.Degree" :model="this.deg" v-if="showDeg" />
+                            <featsSelect :opt="params.PronType" :model="this.pro" v-if="showPronType" />
+                            <featsSelect :opt="params.Clitic" :model="this.cli" v-if="showCli" />
+                            <featsSelect :opt="params.Poss" :model="this.poss" v-if="showPoss" />
+                            <featsSelect :opt="params.Definite" :model="this.def" v-if="showDef" />
+                            <featsSelect :opt="params.NumType" :model="this.numTy" v-if="showNumType" />
                         </div>
                         <div class="float-right pb-4">
                             <button
@@ -94,13 +78,16 @@
 </template>
 
 <script>
+import featsSelect from '../components/featsSelect'
 export default {
     props: {
         featsToEdit: Object,
     },
+    components: { featsSelect },
     data() {
         return {
             showDialog: false,
+            pos: '',
             gen: '',
             num: '',
             ten: '',
@@ -109,6 +96,10 @@ export default {
             mood: '',
             deg: '',
             pro: '',
+            numTy: '',
+            cli: '',
+            def: '',
+            poss: '',
             showGen: false,
             showNum: false,
             showTen: false,
@@ -118,7 +109,10 @@ export default {
             showDeg: false,
             showPronType: false,
             showPoss: false,
-            showClic: false,
+            showCli: false,
+            showNumType: false,
+            showDef: false,
+
             params: {
                 Gender: ['Fem', 'Masc'],
                 Number: ['Sing', 'Plur'],
@@ -131,7 +125,46 @@ export default {
                 Clitic: ['Yes', 'No'],
                 Poss: ['Yes', 'No'],
                 Definite: ['Ind', 'Def'],
+                NumType: ['Card', 'Ord', 'Range', 'Gen'],
             },
+            posList: [
+                { abbr: 'A', full: 'adjective' },
+                { abbr: 'AP', full: 'possessive adjective' },
+                { abbr: 'B', full: 'adverb' },
+                { abbr: 'BN', full: 'negation adverb' },
+                { abbr: 'CC', full: 'coordinative conjunction' },
+                { abbr: 'CS', full: 'subordinative conjunction' },
+                { abbr: 'DE', full: 'exclamative determiner' },
+                { abbr: 'DI', full: 'indefinite determiner' },
+                { abbr: 'DQ', full: 'interrogative determiner' },
+                { abbr: 'DR', full: 'relative determiner' },
+                { abbr: 'DD', full: 'demonstrative determiner' },
+                { abbr: 'E', full: 'preposition' },
+                { abbr: 'FB', full: '“balanced” punctuation' },
+                { abbr: 'FC', full: 'clause boundary punctuation' },
+                { abbr: 'FF', full: 'comma, hyphen' },
+                { abbr: 'FS', full: 'sentence boundary punctuation' },
+                { abbr: 'I', full: 'cardinal number' },
+                { abbr: 'N', full: 'preposition' },
+                { abbr: 'NO', full: 'ordinal number' },
+                { abbr: 'PD', full: 'demonstrative pronoun' },
+                { abbr: 'PE', full: 'personal pronoun' },
+                { abbr: 'PI', full: 'indefinite pronoun' },
+                { abbr: 'PP', full: 'possessive pronoun' },
+                { abbr: 'PQ', full: 'interrogative pronoun' },
+                { abbr: 'PR', full: 'relative pronoun' },
+                { abbr: 'PC', full: 'clitic pronoun' },
+                { abbr: 'RD', full: 'determinative article' },
+                { abbr: 'RI', full: 'indeterminative article' },
+                { abbr: 'S', full: 'common noun' },
+                { abbr: 'SA', full: 'abbreviation' },
+                { abbr: 'SP', full: 'proper noun' },
+                { abbr: 'T', full: 'predeterminer' },
+                { abbr: 'VA', full: 'auxiliary verb' },
+                { abbr: 'VM', full: 'modal verb' },
+                { abbr: 'V', full: 'main verb' },
+                { abbr: 'X', full: 'residual class' },
+            ],
         }
     },
     methods: {
@@ -155,13 +188,21 @@ export default {
         setTimeout(() => {
             this.showDialog = true
         }, 1)
-        this.gen = this.featsToEdit.feats.Gender == undefined ? '' : this.featsToEdit.feats.Gender[0]
-        this.num = this.featsToEdit.feats.Number == undefined ? '' : this.featsToEdit.feats.Number[0]
-        this.ten = this.featsToEdit.feats.Tense == undefined ? '' : this.featsToEdit.feats.Tense[0]
-        this.form = this.featsToEdit.feats.VerbForm == undefined ? '' : this.featsToEdit.feats.VerbForm[0]
-        this.per = this.featsToEdit.feats.Person == undefined ? '' : this.featsToEdit.feats.Person[0]
-        this.mood = this.featsToEdit.feats.Mood == undefined ? '' : this.featsToEdit.feats.Mood[0]
-        this.deg = this.featsToEdit.feats.Degree == undefined ? '' : this.featsToEdit.feats.Degree[0]
+        console.log(this.posList.length)
+        this.pos = this.featsToEdit.pos
+        let features = this.featsToEdit.feats
+        this.gen = features.Gender == undefined ? '' : features.Gender[0]
+        this.num = features.Number == undefined ? '' : features.Number[0]
+        this.ten = features.Tense == undefined ? '' : features.Tense[0]
+        this.form = features.VerbForm == undefined ? '' : features.VerbForm[0]
+        this.per = features.Person == undefined ? '' : features.Person[0]
+        this.mood = features.Mood == undefined ? '' : features.Mood[0]
+        this.deg = features.Degree == undefined ? '' : features.Degree[0]
+        this.pro = features.PronType == undefined ? '' : features.PronType[0]
+        this.cli = features.Clitic == undefined ? '' : features.Clitic[0]
+        this.poss = features.Poss == undefined ? '' : features.Poss[0]
+        this.def = features.Definite == undefined ? '' : features.Definite[0]
+        this.numTy = features.NumType == undefined ? '' : features.NumType[0]
 
         switch (this.featsToEdit.pos) {
             case 'A':
@@ -174,7 +215,124 @@ export default {
                 this.showNum = true
                 this.showPronType = true
                 this.showPoss = true
+                break
+            case 'B':
+                if (this.deg != '') this.showDeg = true
+                break
+            case 'DD' || 'DE' || 'DI' || 'DQ' || 'DR':
+                this.showNum = true
+                this.showPer = true
+                this.showPronType = true
+                break
+            case 'N' || 'NO':
+                this.showNumType = true
+                if (this.num != '') this.showNum = true
+                if (this.gen != '') this.showGen = true
+                break
+            case 'P' || 'PD' || 'PI' || 'PP' || 'PQ' || 'PR':
+                this.showGen = true
+                this.showNum = true
+                this.showPronType = true
+                if (this.gen != '') this.showGen = true
+                break
+            case 'PC' || 'PE':
+                this.showGen = true
+                this.showNum = true
+                this.showPronType = true
+                this.showPer = true
+                if (this.cli != '') this.showCli = true
+                break
+            case 'RD' || 'RI':
+                this.showGen = true
+                this.showNum = true
+                this.showDef = true
+                this.showPronType = true
+                break
+            case 'S' || 'SA':
+                this.showGen = true
+                this.showNum = true
+                break
+            case 'V' || 'VA' || 'VM':
+                this.showForm = true
+                if (this.mood != '') this.showMood = true
+                if (this.num != '') this.showNum = true
+                if (this.per != '') this.showPer = true
+                if (this.tense != '') this.showTen = true
+                break
         }
+    },
+    watch: {
+        pos: function() {
+            console.log('cambia')
+            this.showGen = false
+            this.showNum = false
+            this.showTen = false
+            this.showForm = false
+            this.showPer = false
+            this.showMood = false
+            this.showDeg = false
+            this.showPronType = false
+            this.showPoss = false
+            this.showCli = false
+            this.showNumType = false
+            this.showDef = false
+            switch (this.pos) {
+                case 'A':
+                    this.showGen = true
+                    this.showNum = true
+                    if (this.deg != '') this.showDeg = true
+                    break
+                case 'AP':
+                    this.showGen = true
+                    this.showNum = true
+                    this.showPronType = true
+                    this.showPoss = true
+                    break
+                case 'B':
+                    if (this.deg != '') this.showDeg = true
+                    break
+                case 'DD' || 'DE' || 'DI' || 'DQ' || 'DR':
+                    this.showNum = true
+                    this.showPer = true
+                    this.showPronType = true
+                    break
+                case 'N' || 'NO':
+                    this.showNumType = true
+                    if (this.num != '') this.showNum = true
+                    if (this.gen != '') this.showGen = true
+                    break
+                case 'P' || 'PD' || 'PI' || 'PP' || 'PQ' || 'PR':
+                    this.showGen = true
+                    this.showNum = true
+                    this.showPronType = true
+                    if (this.gen != '') this.showGen = true
+                    break
+                case 'PC' || 'PE':
+                    this.showGen = true
+                    this.showNum = true
+                    this.showPronType = true
+                    this.showPer = true
+                    if (this.cli != '') this.showCli = true
+                    break
+                case 'RD' || 'RI':
+                    this.showGen = true
+                    this.showNum = true
+                    this.showDef = true
+                    this.showPronType = true
+                    break
+                case 'S' || 'SA':
+                    this.showGen = true
+                    this.showNum = true
+                    break
+                case 'V' || 'VA' || 'VM':
+                    this.showForm = true
+                    if (this.mood != '') this.showMood = true
+                    if (this.num != '') this.showNum = true
+                    if (this.per != '') this.showPer = true
+                    if (this.tense != '') this.showTen = true
+                    break
+            }
+        },
     },
 }
 </script>
